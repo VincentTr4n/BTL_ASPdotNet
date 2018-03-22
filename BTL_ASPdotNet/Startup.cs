d35f7046af75bd796dfa8acbdcb562e26a9075a9
VincentTr4n <33866720+VincentTr4n@users.noreply.github.com>
2018-03-22 19:24:56 +0700

    first commit

---
+++ BTL_ASPdotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTL_ASPdotNet.Startup))]
+namespace BTL_ASPdotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
